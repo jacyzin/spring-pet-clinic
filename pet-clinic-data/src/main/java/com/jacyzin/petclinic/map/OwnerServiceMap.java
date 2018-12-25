c2b0b1498205e69c5df4c14642e330ea0a108720
kodemann <henryjacyzin@gmail.com>
2018-12-25 19:02:47 -0200

    Implemented Map based Services. Closes #6

---
+++ pet-clinic-data/src/main/java/com/jacyzin/petclinic/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package com.jacyzin.petclinic.map;
+
+import com.jacyzin.petclinic.model.Owner;
+import com.jacyzin.petclinic.service.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
